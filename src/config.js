7b19a209a3efd985b4d73f7bc943fcdcf276fca5
vol4tim <vol4tim@gmail.com>
2018-10-18 14:12:46 +0400

    up config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
+export const PATH_SSL = ''
 export const PORT = process.env.PORT || 9999
-export const HOST = process.env.HOST || '0.0.0.0'
+export const HOST = process.env.HOST || '0.0.0.0' // '0.0.0.0' // ip4 | ::' // ip6
 export const IPFS_HOST = {
   host: 'localhost',
   port: '5001',
